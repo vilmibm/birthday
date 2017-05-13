de1eeef4823cf7b88498fe9338f1650ce95e232c
nathaniel smith <nathanielksmith@gmail.com>
2017-05-13 14:09:29 -0700

    move arg processing into function

--- birthday.c
+++ birthday.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include <time.h>
 
-// check for argument, picking default if needed
 // check each home dir for .birthday file
     // collect matches
 // print matches
@@ -11,25 +10,18 @@
 
 char valid_date(char *date) {
   int converted, month, day;
-  converted = sscanf(date, "%2d/%2d", &month, &day);
-  if (converted != 2) {
-    return 0;
-  }
-  if (month < 1 || month > 12) {
-    return 0;
-  }
-  if (day < 1 || day > 31) {
-    return 0;
-  }
+  char extra[1];
+  if (strlen(date) > 5) return 0;
+  converted = sscanf(date, "%2d/%2d%1c", &month, &day, extra);
+  if (converted != 2) return 0;
+  if (month < 1 || month > 12) return 0;
+  if (day < 1 || day > 31) return 0;
   return 1;
 }
 
-
-int main(int argc, char *argv[]) {
-  char *month_day;
+void process_args(const int argc, char *argv[], char *month_day) {
   time_t now;
   struct tm *utc;
-
   if (argc == 1) {
     time(&now);
     utc = gmtime(&now);
@@ -46,7 +38,12 @@ int main(int argc, char *argv[]) {
     fprintf(stderr, "bad number of args passed: %d\n", argc);
     exit(2);
   }
+}
 
+int main(int argc, char *argv[]) {
+  char month_day[5];
+
+  process_args(argc, argv, month_day);
   puts(month_day);
 
   return 0;
